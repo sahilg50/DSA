6955fb82008e98333ea33339e9913933783c0a93
Sahil <56653026+sahilg50@users.noreply.github.com>
2022-07-11 02:59:35 +0530

    Add: Largest and second largest element in the array

---
+++ ARRAY/2ndLargestElement.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// -inf < nums[i] < +inf
+void largest_secondLargest(vector<int> &nums)
+{
+    if (nums[0] > nums[1])
+        swap(nums[0], nums[1]);
+
+    int fl = nums[1];
+    int sl = nums[0];
+
+    for (int i = 2; i < nums.size(); i++)
+    {
+        if (nums[i] > fl)
+        {
+            sl = fl;
+            fl = nums[i];
+        }
+        else if (nums[i] < fl)
+            sl = max(sl, nums[i]);
+    }
+
+    cout << "\nLargest: " << fl << "\nSecond largest: " << sl;
+
+    /*
+    TC-> O(N), single pass of array
+    SC-> O(1), constant
+    */
+}
+
+int main()
+{
+    vector<int> nums = {100, 4, 6, 1, 9, -2, 0, 10};
+    largest_secondLargest(nums);
+    return 0;
+}
