fa06fadbf6bcd71d2a39028fc9f36ee3bd4e0c19
Sahil <56653026+sahilg50@users.noreply.github.com>
2022-08-09 12:43:31 +0530

    Add: Koko Eating Bananas

---
+++ GFG/Koko_Eating_Bananas.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool isValid(vector<int> &piles, int mid_val, int H)
+{
+    int time = 0;
+    for (int i = 0; i < piles.size(); i++)
+    {
+        if (piles[i] % mid_val != 0)
+            time += (piles[i] / mid_val) + 1;
+
+        else
+            time += piles[i] / mid_val;
+    }
+
+    return time <= H;
+}
+
+int minEatingSpeed(vector<int> &piles, int H)
+{
+    int start = *min_element(piles.begin(), piles.end());
+    int end = *max_element(piles.begin(), piles.end());
+    while (start < end)
+    {
+        int mid = (start + end) / 2;
+
+        if (isValid(piles, mid, H))
+            end = mid;
+        else
+            start = mid + 1;
+    }
+    return end;
+}
+
+int main()
+{
+
+    vector<int> piles = {30, 11, 23, 4, 20};
+    int H = 6; // Number of hours
+    cout << minEatingSpeed(piles, H);
+    return 0;
+}
+
+/*
+TC-> O(log N), N = max(arr) - min(arr)
+SC-> O(I)
+*/
