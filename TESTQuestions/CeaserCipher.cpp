100b5ebb93ec47932bdf3245f22c578cf7d2f1d5
Sahil <56653026+sahilg50@users.noreply.github.com>
2022-09-02 18:30:25 +0530

    ADD: Ceaser Cipher

--- TESTQuestions/CeaserCipher.cpp
+++ TESTQuestions/CeaserCipher.cpp
@@ -7,12 +7,12 @@ It is given in the question that cipher was formed by replacing the string alpha
 #include <unordered_map>
 using namespace std;
 
+// Approach 1-> Maps
 char *decrypt(char *input1)
 {
     unordered_map<char, char> m;
     for (char ch = 'a'; ch <= 'z'; ch++)
     {
-        cout << ch;
         if (ch >= 'x')
         {
             if (ch == 'x')
@@ -32,15 +32,33 @@ char *decrypt(char *input1)
     }
     return input1;
 }
+
+// Approach 2-> Maths
+char *decrypt2(char *input1)
+{
+    char *t;
+    for (t = input1; *t != '\0'; t++)
+    {
+        *t = char(((*t - 'a' + 3) % 26) + 97);
+    }
+    return input1;
+}
 int main()
 {
     char name[] = "nrfzh";
     decrypt(name);
-    cout << endl
-         << endl;
     for (int i = 0; i < sizeof(name); i++)
     {
         cout << name[i];
+    }
+    cout << endl;
+
+    // Checking the second approach
+    char name1[] = "nrfzh";
+    decrypt2(name1);
+    for (int i = 0; i < sizeof(name1); i++)
+    {
+        cout << name1[i];
     }
     return 0;
 }
