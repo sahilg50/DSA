d2b05c243d122a814bbb63c46203193a80eedee5
Sahil <56653026+sahilg50@users.noreply.github.com>
2022-09-20 01:39:33 +0530

    Add: Conversion operator overloading

---
+++ OOPS/Polymorphism/conversionOperatorOverloading.cpp
@@ -0,0 +1,32 @@
+/*
+Conversion Operator: We can also write conversion operators that can be used to convert one type to another type.
+*/
+
+#include <iostream>
+using namespace std;
+class Fraction
+{
+private:
+    int num, den;
+
+public:
+    Fraction(int n, int d)
+    {
+        num = n;
+        den = d;
+    }
+
+    // Conversion operator: return float value of fraction
+    operator float() const
+    {
+        return float(num) / float(den);
+    }
+};
+
+int main()
+{
+    Fraction f(2, 5);
+    float val = f;
+    cout << val << '\n';
+    return 0;
+}
--- OOPS/Polymorphism/operatorOverloading.cpp
+++ OOPS/Polymorphism/operatorOverloading.cpp
@@ -1,99 +1,35 @@
-// https : // www.youtube.com/watch?v=i_5pvt7ag7E&t=494s
-// https://www.codingninjas.com/codestudio/guided-paths/basics-of-c/content/118817/offering/1381799
 #include <iostream>
 using namespace std;
 
-/*
- */
-
-class Hero
+class ComplexNumber
 {
+private:
+    int real;
+    int imaginary;
 
 public:
-    // properties
-    char *name;
-    int level;
-    int health;
-    static int timeToComplete;
-
-    // Non-Parameterized constructor
-    Hero()
+    ComplexNumber(int real, int imaginary)
     {
-        cout << "Simple construtor is called " << endl;
+        this->real = real;
+        this->imaginary = imaginary;
     }
-    // Parameterized constructor
-    Hero(char name, int level, int health)
-    {
-        cout << "Construtor called.\n";
-        this->name = new char[1];
-        this->name[0] = name;
-        this->level = level;
-        this->health = health;
-    }
-    // Copy Constructor
-    Hero(Hero &temp)
-    {
-        cout << "Copy constructor called" << endl;
-        this->name = temp.name;
-        this->level = temp.level;
-        this->health = temp.health;
-    }
-    // Method to print all the properties
     void print()
     {
-        cout << "[ Name: " << this->name[0] << ", ";
-        cout << "Level: " << this->level << ", ";
-        cout << "Health: " << this->health << ", ";
-        cout << "Time To Complete: " << timeToComplete << " ]";
+        cout << real << " + i" << imaginary;
     }
-
-    // Static member function
-    static int gotHit()
+    ComplexNumber operator+(const ComplexNumber &c2)
     {
-        timeToComplete /= 2;
-    }
-    ~Hero()
-    {
-        cout << "Destructor called for: " << this->name[0] << endl;
+        ComplexNumber c3(0, 0);
+        c3.real = this->real + c2.real;
+        c3.imaginary = this->imaginary + c2.imaginary;
+        return c3;
     }
 };
-
-// Assigning value to the static variable
-int Hero::timeToComplete = 100;
-
 int main()
 {
-    // Statically allocated
-    Hero A('A', 1, 20);
-    cout << "Size of the class Hero: " << sizeof(A) << endl;
-    A.print();
-    cout << "\n\n";
-
-    // Dynamically allocated
-    Hero *B = new Hero('B', 2, 40);
-    // (*B).print();
-    B->print();
-    cout << endl;
-    delete B;
-    cout << "\n\n";
-
-    // Create C as a shallow copy of A
-    Hero C(A);
-    C.print();
-    cout << "\n\n";
-
-    // When copying the data of object at the time of initialization then copy is made through COPY CONSTRUCTOR
-    Hero D = A;
-    D.timeToComplete = 300; // Updating the value of the static variable
-    D.print();
-    cout << "\n\n";
-
-    // When copying the data of object after initialization then the copy is done through DEFAULT ASSIGNMENT OPERATOR
-    Hero E;
-    E = A;
-    E.gotHit(); // Static method to change the value of the static variable
-    E.print();
-    cout << "\n\n";
-    // In all the above examples, a shallow copy is being created since some of the data members have been dynammically allocated.
+    ComplexNumber c1(3, 5);
+    ComplexNumber c2(2, 4);
+    ComplexNumber c3 = c1 + c2;
+    c3.print();
     return 0;
 }
