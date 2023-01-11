3ea365143f092c843bec13817a85c716d40dc15e
Sahil <56653026+sahilg50@users.noreply.github.com>
2023-01-11 14:15:38 +0530

    Add: 44. Wildcard Matching

--- rough.cpp
+++ rough.cpp
@@ -5,12 +5,14 @@ using namespace std;
 
 int main()
 {
-    string str = "gaga";
-    map<char, int> M;
-    for (char ch = 'a'; ch != 'z'; ch++)
-        M[ch] = 0;
+    // string str = "gaga";
+    // map<char, int> M;
+    // for (char ch = 'a'; ch != 'z'; ch++)
+    //     M[ch] = 0;
 
-    for (auto &it : M)
-        cout << it.first << " " << it.second << endl;
+    // for (auto &it : M)
+    //     cout << it.first << " " << it.second << endl;
+    if (-1)
+        cout << bool(0);
     return 0;
 }
