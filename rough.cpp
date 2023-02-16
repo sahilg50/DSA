d1e5571cde7db1fd9b186d693e963223431de001
Sahil <56653026+sahilg50@users.noreply.github.com>
2023-02-16 14:13:45 +0530

    Add: 2477. Minimum Fuel Cost to Report to the Capital

--- rough.cpp
+++ rough.cpp
@@ -1,22 +1,9 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <unordered_map>
-#include <algorithm>
+#include <cmath>
 using namespace std;
 
-int doesSatisfy(int val)
-{
-    return val == 30;
-}
 int main()
 {
-    vector<int> vec = {10, 20, 30, 40, 30, 60, 70, 30, 100};
-    // auto it = remove_if(vec.begin(), vec.end(), doesSatisfy);
-    // vec.erase(it, vec.end());
-    vec.pop_back();
-    for (auto num : vec)
-        cout << num << " ";
-
+    cout << 1.0 * 5 / 2;
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,2 +1,14 @@
-for (auto it : M)
-    //     cout << it.first << " " << it.second << endl;
+#include <iostream>
+using namespace std;
+
+int myFunc(int n)
+{
+    if (n != 10)
+        return n;
+}
+
+int main()
+{
+    myFunc(10);
+    return 0;
+}
