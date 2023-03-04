1990fe75e6c519055cb1a7c8093e781e10ef0ff1
Sahil <56653026+sahilg50@users.noreply.github.com>
2023-03-04 16:47:12 +0530

    Add: 2444. Count Subarrays With Fixed Bounds

--- rough.cpp
+++ rough.cpp
@@ -1,33 +1,8 @@
 #include <iostream>
 using namespace std;
 
-class Hero
-{
-private:
-    char name;
-
-public:
-    Hero(char ch) { this->name = ch; }
-
-    Hero &set()
-    {
-        cout << "\nSet method called!";
-        // Return the object which called this method as reference.
-        return *this;
-    }
-
-    void get() { cout << endl
-                      << this->name; }
-};
-
 int main()
 {
-
-    Hero A('a');
-    Hero B('b');
-    A = B.set();
-    A.get();
-
-    cout << '9' - '0';
+    cout << !(5 < 9);
     return 0;
 }
