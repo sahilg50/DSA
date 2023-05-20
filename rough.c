68ca2431fe3f6fdd15d8a83a99de2597d0b5b51c
Sahil Gupta <sahilgupta@ip-192-168-1-6.ap-south-1.compute.internal>
2023-05-21 02:02:00 +0530

    Add: Macbook updates

--- rough.c
+++ rough.c
@@ -2,11 +2,11 @@
 
 int main()
 {
-   int num[] = {1,4,8,12,16};
+   int num[] = {1, 4, 8, 12, 16};
    int *a, *b;
    int i;
    a = num;
-   b= num+2;
-   i=*a+1;
-   printf("%d, %d, %d",i,*a,*b);
+   b = num + 2;
+   i = *a + 1;
+   printf("%d, %d, %d", i, *a, *b);
 }
