6bfd12345ef656b8eb47f8085ab7d12e445ca322
Sahil Gupta <sahilgupta.312000@gmail.com>
2023-07-15 17:30:19 +0530

    Saturday's lecture with Anurag

--- rough.cpp
+++ rough.cpp
@@ -3,51 +3,46 @@
 #include <string>
 #include <vector>
 #include <math.h>
+#include <algorithm>
 
 using namespace std;
 
-// Using tabulation method to form the dp table and then using it to print the lcs string.
-string lcs(const string &s, const string &t)
+long packetsRemaining(int totalPackets, int time, int rate)
 {
-    int n = s.size(), m = t.size();
-    vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-
-    for (int i = n; i >= 0; i--)
+    long maxTransferPossible = time * rate;
+    if (totalPackets <= maxTransferPossible)
     {
-        for (int j = m; j >= 0; j--)
-        {
-
-            if (i >= n || j >= m)
-                dp[i][j] = 0;
-
-            else if (s[i] == t[j])
-                dp[i][j] = 1 + dp[i + 1][j + 1];
-
-            else
-                dp[i][j] = max(dp[i][j + 1], dp[i + 1][j]);
-        }
+        return 0;
     }
-    // cout << dp[0][0];
-
-    string ans = "";
-    int i = 0, j = 0;
-    while (i < n && j < m)
-    {
-        if (s[i] == t[j])
-        {
-            ans.push_back(s[i]);
-            i++, j++;
-        }
-        else if (dp[i][j + 1] > dp[i + 1][j])
-            j++;
-        else
-            i++;
-    }
-    return ans;
+    return totalPackets - maxTransferPossible;
 }
-
+long getNumberOfDroppedPackets(vector<vector<int>> requests, int max_packets, int rate)
+{
+    sort(requests.begin(), requests.end());
+    long packetsDropped = 0, packetsPending = 0;
+    int i;
+    for (i = 0; i < requests.size() - 1; i++)
+    {
+        long time = requests[i + 1][0] - requests[i][0];
+        long totalPackets = packetsPending + requests[i][1];
+        if (totalPackets > max_packets)
+        {
+            packetsDropped += totalPackets - max_packets;
+            packetsPending = packetsRemaining(max_packets, time, rate);
+        }
+        else
+        {
+            packetsPending = totalPackets - (time * rate);
+        }
+    }
+    long totalPackets = packetsPending + requests[i][1];
+    if (totalPackets > max_packets)
+    {
+        packetsDropped += totalPackets - max_packets;
+    }
+    return packetsDropped;
+}
 int main()
 {
-    cout << lcs("sahil", "shl");
     return 0;
 }
