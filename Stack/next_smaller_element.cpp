b9d87ec21313efe01dc357c2dede1697e47e981b
Sahil Gupta <sahilgupta@192.168.1.14>
2023-10-02 02:10:12 +0530

    Revise: Next smaller element in an array

--- Stack/next_smaller_element.cpp
+++ Stack/next_smaller_element.cpp
@@ -4,17 +4,18 @@
 #include <stack>
 using namespace std;
 
-vector<int> NSE(int *arr, int n)
+// Approach 1: Using Stack (Left -> Right traversal)
+vector<int> nextSmallerElement(int *arr, int n)
 {
     // storing the next smaller values in nse
-    vector<int> nse(n, 0);
+    vector<int> NSE(n, -1);
     stack<int> S;
     int i = 0;
     while (i < n)
     {
         if ((!S.empty()) && (arr[S.top()] > arr[i]))
         {
-            nse[S.top()] = arr[i];
+            NSE[S.top()] = arr[i];
             S.pop();
         }
         else
@@ -22,16 +23,49 @@ vector<int> NSE(int *arr, int n)
             S.push(i++);
         }
     }
-    return nse;
+    return NSE;
+}
+/*
+TC-> O(2*N), because at any time you'll push and pop each element
+SC-> Stack: O(N) + NSE: O(N)ASC
+*/
+
+// Approach 2: Using Stack (Right -> Left Traversal)
+vector<int> nextSmallerElement2(int *arr, int n)
+{
+
+    // Storing the next smaller values in nse
+    vector<int> NSE(n, -1);
+    stack<int> S;
+    int i = n - 1;
+    while (i >= 0)
+    {
+        if (!S.empty() && S.top() < arr[i])
+        {
+            NSE[i] = S.top();
+            S.push(arr[i]);
+            i--;
+        }
+        else if (S.empty())
+        {
+            S.push(arr[i]);
+            i--;
+        }
+        else
+        {
+            S.pop();
+        }
+    }
+    return NSE;
 }
 
 int main()
 {
     // array containing only positive elements
-    int arr[] = {2, 90, 3, 4, 3, 1};
+    int arr[] = {2, 0, 90, 80, 3, 2, 4, 3, 1};
     int n = sizeof(arr) / sizeof(int);
 
-    vector<int> nse = NSE(arr, n);
+    vector<int> nse = nextSmallerElement2(arr, n);
 
     cout << "\nNSE: ";
     for (auto i : nse)
--- rough.cpp
+++ rough.cpp
@@ -2,41 +2,13 @@
 #include <vector>
 using namespace std;
 
-int solution(vector<int> &number)
-{
-    int N = number.size() / 2;
-    int count = 0;
-
-    for (int i = 0; i < N - 1; i++)
-    {
-        int left1 = number[2 * i];
-        int right1 = number[2 * i + 1];
-        int left2 = number[2 * (i + 1)];
-        int right2 = number[2 * (i + 1) + 1];
-
-        if (right1 != left2)
-        {
-            count++;
-
-            if (left1 + right1 < left2 + right2)
-            {
-                number[2 * (i + 1)] = left1;
-                number[2 * (i + 1) + 1] = right1;
-            }
-            else
-            {
-                number[2 * i] = left2;
-                number[2 * i + 1] = right2;
-            }
-        }
-    }
-    return count;
-}
-
 int main()
 {
-    vector<int> dominoes = {2, 4, 1, 3, 4, 6, 2, 4, 1, 6};
-    int result = solution(dominoes);
-    cout << "Minimum number of tiles to remove: " << result << endl;
+    vector<int> numbers = {2, 4, 1, 3, 4, 6, 2, 4, 1, 6};
+    for (const int &number : numbers)
+        cout << number << " "
+             << "Akul"
+             << endl;
+
     return 0;
 }
