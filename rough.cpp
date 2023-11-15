aacb183e3132ca8be1c9fae3fcb6669467c6e11a
Sahil Gupta <sahilgupta@192.168.1.2>
2023-11-15 22:40:20 +0530

    Add: 1930. Unique Length-3 Palindromic Subsequences

--- rough.cpp
+++ rough.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
+#include <string>
 #include <vector>
 using namespace std;
 
 int main()
 {
-    vector<int> numbers = {2, 4, 1, 3, 4, 6, 2, 4, 1, 6};
-    for (const int &number : numbers)
-        cout << number << " "
-             << "Akul"
-             << endl;
+    // vector<int> numbers = {2, 4, 1, 3, 4, 6, 2, 4, 1, 6};
+    // for (const int &number : numbers)
+    //     cout << number << " "
+    //          << "Akul"
+    //          << endl;
 
+    string sett = "" + 'a';
+    cout << sett;
     return 0;
 }
