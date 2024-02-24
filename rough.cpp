c82b3309689324b851a3b7f1ac59836f3003004d
Sahil Gupta <sahilgupta@192.168.1.5>
2024-02-24 13:35:57 +0530

    Update: 144. Binary Tree Preorder Traversal

--- rough.cpp
+++ rough.cpp
@@ -3,18 +3,83 @@
 #include <algorithm>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 using namespace std;
 
+vector<vector<int>> v;
+vector<int> vis, par, tmp;
+long long dfs(int node, int p = -1)
+{
+    vis[node] = 1;
+    par[node] = p;
+    tmp.push_back(node);
+    for (auto i : v[node])
+    {
+        if (vis[i] == 0)
+        {
+            long long z = dfs(i, node);
+            if (z != -1)
+            {
+                return z;
+            }
+        }
+        else if (vis[i] == 1)
+        {
+            long long sum = i;
+            while (node != i)
+            {
+                sum += node;
+                node = par[node];
+            }
+            if (node == i)
+                return sum;
+            return -1;
+        }
+    }
+    return -1;
+}
+
+// Function to find largest sum cycle
+long long largestSumCycle(int N, vector<int> Edge)
+{
+    long long ans = -1;
+    vis = vector<int>(N);
+    v = vector<vector<int>>(N);
+    par = vector<int>(N);
+
+    for (int i = 0; i < N; i++)
+    {
+        if (Edge[i] != -1)
+        {
+            v[i].push_back(Edge[i]);
+        }
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        if (!vis[i])
+        {
+            ans = max(ans, dfs(i));
+            for (auto j : tmp)
+            {
+                vis[j] = 2;
+            }
+            tmp.clear();
+        }
+    }
+
+    return ans;
+}
+
+// Drivers code
 int main()
 {
-    unordered_map<int, string> M;
 
-    string value = "Hello";
-    M.insert({1, value});
-    cout << M[1] << endl;
+    int N = 4;
+    vector<int> Edge = {1, 2, 0, -1};
 
-    value = "Sahil";
-    cout << M[1];
-
+    // Function Call
+    int ans = largestSumCycle(N, Edge);
+    cout << ans;
     return 0;
 }
--- rough.py
+++ rough.py
@@ -1,3 +1,74 @@
-for i in range(1, 11):
-    i = i>>1
-    print(i)
+class Solution():
+
+    #Method to perform DFS
+    def dfs(self, node,p=-1):
+        self.visited[node]=1
+        self.parent[node]=p
+        self.temporary.append(node)
+
+        # Iterate the connecting nodes
+        for i in self.v[node]:
+                
+            #if already visited
+            if(self.visited[i]==1):
+
+                sum=i
+                #calculating cycle sum
+                while(node!=i):
+                    sum+=node
+                    node=self.parent[node]
+                
+                # if cycle found
+                if(node==i): return sum
+
+                return -1
+            
+            elif(self.visited[i]==0):
+                z=self.dfs(i,node)
+                if(z!=-1): return z
+            
+        return -1
+
+    #Function to find cycle sum
+    def largestSumCycle(self, N, Edge):
+        result=-1
+        self.visited=[0]*N
+        self.v=[[] for i in range(N)]
+        self.parent = [0]*N
+        self.temporary = []
+
+        #create  graph
+        for i in range(N):
+            if(Edge[i]!=-1):
+                self.v[i].append(Edge[i])
+
+        #iterate all nodes in graph
+        for i in range(N):
+            
+            #if a node not visited, then perform dfs
+            if(~self.visited[i]):
+                result=max(result,self.dfs(i))
+
+                for j in self.temporary: self.visited[j]= 2
+                
+                self.temporary.clear()
+
+        return result
+    
+if __name__ == '__main__':
+
+    T = input()
+    N = int(input())
+    Edge = input().split(' ')
+
+    Arr = []
+    for i in range(len(Edge)):
+        Arr.append(int(Edge[i]))
+
+    Edge = Arr
+    
+    obj = Solution()
+
+    result = obj.largestSumCycle(N, Edge)
+
+    print(result)
