1c6e18a26076f1c0ef2d76b3c2678c6ef0aaf161
Sahil Gupta <sahilgupta@192.168.1.2>
2024-08-17 01:52:27 +0530

    Add: 12. Integer to Roman

--- rough.cpp
+++ rough.cpp
@@ -8,9 +8,8 @@ using namespace std;
 
 int main()
 {
-    string str = "sahil->";
-    str.pop_back();
-    str.pop_back();
-    cout << str;
+    vector<pair<char, int>> temp = {{'A', 10}, {'B', 20}, {'C', 30}};
+    for (auto it = temp.rbegin(); it != temp.rend(); it++)
+        cout << (*it).first * 2;
     return 0;
 }
