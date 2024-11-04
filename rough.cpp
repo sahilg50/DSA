825c5a5ec7396659624a382711d949d2ec823351
Sahil Gupta <sahilgupta@192.168.1.5>
2024-11-05 00:48:29 +0530

    Add: 2035. Partition Array Into Two Arrays to Minimize Sum Difference

--- rough.cpp
+++ rough.cpp
@@ -1,16 +1,39 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <cctype>
+/*
 
+TOPIC: Proof that static variables' lifetime (or "extent") is the entire run of the program.
+    -> In the code we declare a class and then create an object from it inside the while loop. So the destructor should be called as soon as the while loop ends. But this does not happen because it is a static object and it is only destroyed when the main function ends.
+
+    -> But if we make the object non-static then the destructor gets called as soon as the while loop ends.
+
+*/
+#include <iostream>
 using namespace std;
+
+class ABC
+{
+private:
+    int i = 0;
+
+public:
+    ABC()
+    {
+        i = 0;
+        cout << "Inside Constructor\n";
+    }
+    ~ABC()
+    {
+        i = 0;
+        cout << "Inside Destructor\n";
+    }
+};
 
 int main()
 {
-    vector<int> dp = {1, 2, 3, 4, 4};
-    dp = {0};
-    for (auto num : dp)
-        cout << num << " ";
+    while (1)
+    {
+        ABC temp;
+        break;
+    }
+    cout << "End Of Main\n";
     return 0;
 }
