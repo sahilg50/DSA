46cadba4d7888d4ecd58190ab89979a75f0fc6a2
Sahil Gupta <sahilgupta@192.168.1.5>
2024-11-07 20:43:36 +0530

    Add: Partitions with Given Difference

--- rough.cpp
+++ rough.cpp
@@ -1,39 +1,11 @@
-/*
-
-TOPIC: Proof that static variables' lifetime (or "extent") is the entire run of the program.
-    -> In the code we declare a class and then create an object from it inside the while loop. So the destructor should be called as soon as the while loop ends. But this does not happen because it is a static object and it is only destroyed when the main function ends.
-
-    -> But if we make the object non-static then the destructor gets called as soon as the while loop ends.
-
-*/
 #include <iostream>
 using namespace std;
 
-class ABC
-{
-private:
-    int i = 0;
-
-public:
-    ABC()
-    {
-        i = 0;
-        cout << "Inside Constructor\n";
-    }
-    ~ABC()
-    {
-        i = 0;
-        cout << "Inside Destructor\n";
-    }
-};
-
 int main()
 {
-    while (1)
-    {
-        ABC temp;
-        break;
-    }
-    cout << "End Of Main\n";
+    int a = 5;
+    int b = 6;
+    int c = a == b;
+    cout << c;
     return 0;
 }
