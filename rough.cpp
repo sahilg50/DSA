a30e452332e5953bc5d4c1933511d3c696117653
Sahil Gupta <sahilgupta@192.168.1.3>
2025-03-06 02:06:15 +0530

    Add: 36. Valid Sudoku

--- rough.cpp
+++ rough.cpp
@@ -5,7 +5,19 @@ using namespace std;
 
 int main()
 {
-    cout << INT_MAX << endl;
-    cout << (5 << 1);
+    vector<int> vec = {1, 0, 0, 9, 9, 3, 0, 1, 0, 0, -1, 10, 9, 4, 5, 6, 6, 0, 0, 0, 0, -10};
+    int mask = 0;
+    for (int i = 0; i < vec.size(); i++)
+    {
+        if (vec[i] != 0)
+            mask |= (1 << i);
+    }
+
+    for (int i = 0; i < vec.size(); i++)
+    {
+        if (mask & (1 << i))
+            cout << vec[i] << " ";
+    }
+
     return 0;
 }
